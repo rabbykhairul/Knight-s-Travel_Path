bfbce9ee72e37d073f944e88e80f95eff3035372
rabbykhairul <rabbykhairul@gmail.com>
2019-09-17 21:02:48 +0600

    Add bfs dfs search algorithm

--- poly_tree_node.rb
+++ poly_tree_node.rb
@@ -28,4 +28,27 @@ class PolyTreeNode
     def add_child(child_node)
         child_node.parent = self
     end
+
+    def bfs(target_value)
+        queue = [ self ]
+
+        until queue.empty?
+            current_node = queue.shift
+            return current_node if target_value == current_node.value
+            current_node.children.each do |child_node|
+                queue.push(child_node)
+            end
+        end
+
+        nil
+    end
+
+    def dfs(target_value)
+        return self if self.value == target_value
+        self.children.each do |child_node|
+            search_result = child_node.dfs(target_value)
+            return search_result unless search_result.nil?
+        end
+        nil
+    end
 end
